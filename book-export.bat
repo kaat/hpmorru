333faee4602f17f9298635c21c92bce92a3e2da6
yuliyl <yuliyl@40b4540e-9097-e14a-8178-55e7c3b43524>
2013-11-20 10:15:55 +0000

    git-svn-id: file:///home/yuu/windows/hpmor/branches/hpmorrepo@11 40b4540e-9097-e14a-8178-55e7c3b43524

--- book-export.bat
+++ book-export.bat
@@ -5,7 +5,11 @@
 @goto :EOF
 #>
 
-$null = new-item "./export/html" -type directory -force
+$pandoc = "C:\Users\yuliyl.EA\AppData\Local\Pandoc\pandoc.exe" # переменная для полного пути к pandoc, требуется, если путь к нему не находится в Env:Path
+$7z = "C:\Program Files (x86)\7-Zip\7z.exe"
+
+if(test-path("./export/html/")) { $null = remove-item "./export/html/" -force -recurse }
+$null = new-item "./export/html/" -type directory -force
 
 copy "./markdown.css" -destination "./export/html/"
 copy "./markdown.css" -destination "./export/"
@@ -14,28 +18,24 @@ copy "./markdown.css" -destination "./export/"
 dir "./parts" -filter "*.md" | % {
    "  * $($_.Name)"
    $output = "./export/html/" + $_.Name + ".html"
-   C:\Users\yuliyl.EA\AppData\Local\Pandoc\pandoc.exe --from=markdown --smart --standalone --self-contained --css=markdown.css --output=$output $_.FullName
+   &$pandoc --from=markdown --smart --standalone --self-contained --css=markdown.css --output=$output $_.FullName
 }
 
 "Конвертируем в EPUB..."
-C:\Users\yuliyl.EA\AppData\Local\Pandoc\pandoc.exe --from=markdown --smart --output=export/hpmor_ru.epub --epub-cover-image=images/cover.jpg "./export/hpmor_ru.md"
+&$pandoc --from=markdown --smart --output=export/hpmor_ru.epub --epub-cover-image=images/cover.jpg "./export/hpmor_ru.md"
 # "Конвертируем в PDF..."
-# C:\Users\yuliyl.EA\AppData\Local\Pandoc\pandoc.exe --from=markdown --to=pdf --output=export/hpmor_ru.pdf "./export/hpmor_ru.md"
+# &$pandoc --from=markdown --to=pdf --output=export/hpmor_ru.pdf "./export/hpmor_ru.md"
 
 "Конвертируем в HTML..."
-C:\Users\yuliyl.EA\AppData\Local\Pandoc\pandoc.exe --from=markdown --smart --standalone --self-contained --css=markdown.css --output=export/hpmor_ru.html "./export/hpmor_ru.md"
+&$pandoc --from=markdown --smart --standalone --self-contained --css=markdown.css --output=export/hpmor_ru.html "./export/hpmor_ru.md"
 $content = [System.IO.File]::ReadAllText("./export/hpmor_ru.html")
 $content = $content.Replace("./images/", "../images/")
 $content | Out-File "./export/hpmor_ru.html"
 
 "Конвертируем в FB2..."
-C:\Users\yuliyl.EA\AppData\Local\Pandoc\pandoc.exe --from=markdown --smart --output=export/hpmor_ru.fb2 "./export/hpmor_ru.md"
-zip -9 -m -D -j "./export/hpmor_ru.fb2.zip" "./export/hpmor_ru.fb2" # делаем архив
+&$pandoc --from=markdown --smart --output=export/hpmor_ru.fb2 "./export/hpmor_ru.md"
+&$7z a -tzip -mx9 "./export/hpmor_ru.fb2.zip" "./export/hpmor_ru.fb2" # делаем архив
 "Конвертируем в DOCX..."
-C:\Users\yuliyl.EA\AppData\Local\Pandoc\pandoc.exe --from=markdown --smart --output=export/hpmor_ru.docx "./export/hpmor_ru.md"
+&$pandoc --from=markdown --smart --output=export/hpmor_ru.docx "./export/hpmor_ru.md"
 "Конвертируем в MOBI..."
-C:\Users\yuliyl.EA\AppData\Local\Pandoc\pandoc.exe --from=markdown --smart --output=export/hpmor_ru.mobi "./export/hpmor_ru.md"
-
-"Export completed!"
-
-# read-host
+&$pandoc --from=markdown --smart --output=export/hpmor_ru.mobi "./export/hpmor_ru.md"
