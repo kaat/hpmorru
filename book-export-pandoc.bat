472aa68304f9be03a0f671d1668d311e51b1f88f
yuliyl <yuliyl@40b4540e-9097-e14a-8178-55e7c3b43524>
2013-11-27 13:05:17 +0000

    git-svn-id: file:///home/yuu/windows/hpmor/branches/hpmorrepo@21 40b4540e-9097-e14a-8178-55e7c3b43524

--- book-export-pandoc.bat
+++ book-export-pandoc.bat
@@ -13,6 +13,7 @@ if(test-path("C:/Users/yuliyl.EA/AppData/Local/Pandoc/pandoc.exe")) {
 
 $7z = "C:/Program Files (x86)/7-Zip/7z.exe"
 $css = "./images/markdown.css"
+$name = "hpmor_ru_pandoc"
 
 if(test-path("./export/html/")) { $null = remove-item "./export/html/" -force -recurse }
 $null = new-item "./export/html/" -type directory -force
@@ -20,36 +21,36 @@ $null = new-item "./export/html/" -type directory -force
 copy "./images/*.css" -destination "./export/html/"
 copy "./images/*.css" -destination "./export/"
 
-"Конвертируем в HTML по главам..."
-@(dir "./parts" -filter "*.md") +
-@(dir "./parts_include" -filter "*.md") +
-@(dir "./parts_include_pandoc" -filter "*.md") |
-% {
-   "  * $($_.Name)"
-   $output = "./export/html/" + $_.Name + ".html"
-   &$pandoc --from=markdown --smart -V lang:russian --standalone --self-contained --css=$css --output=$output $_.FullName
-}
+# "Конвертируем в HTML по главам..."
+# @(dir "./parts" -filter "*.md") +
+# @(dir "./parts_include" -filter "*.md") +
+# @(dir "./parts_include_pandoc" -filter "*.md") |
+# % {
+#    "  * $($_.Name)"
+#    $output = "./export/html/" + $_.Name + ".html"
+#    &$pandoc --from=markdown --smart -V lang:russian --standalone --self-contained --css=$css --output=$output $_.FullName
+# }
 
 "Конвертируем в EPUB..."
-&$pandoc --from=markdown --smart -V lang:russian --output=export/hpmor_ru.epub --epub-cover-image=images/cover.jpg "./export/hpmor_ru_pandoc.md"
+&$pandoc --from=markdown --smart -V lang:russian --output="./export/$name.epub" --epub-cover-image=images/cover.jpg "./export/$name.md"
 # "Конвертируем в PDF..."
-# &$pandoc --from=markdown --to=pdf --output=export/hpmor_ru.pdf "./export/hpmor_ru_pandoc.md"
+# &$pandoc --from=markdown --to=pdf --output="./export/$name.pdf" "./export/$name.md"
 
 "Конвертируем в HTML..."
-&$pandoc --from=markdown --smart -V lang:russian --standalone --self-contained --css=$css --output=export/hpmor_ru.html "./export/hpmor_ru_pandoc.md"
-$content = [System.IO.File]::ReadAllText("./export/hpmor_ru.html")
+&$pandoc --from=markdown --smart -V lang:russian --standalone --self-contained --css=$css --output="./export/$name.html" "./export/$name.md"
+$content = [System.IO.File]::ReadAllText("./export/$name.html")
 $content = $content.Replace("./images/", "../images/")
-$content | Out-File "./export/hpmor_ru.html"
+$content | Out-File "./export/$name.html"
 
 "Конвертируем в FB2..."
-&$pandoc --from=markdown --smart -V lang:russian --output=export/hpmor_ru.fb2 "./export/hpmor_ru_pandoc.md"
+&$pandoc --from=markdown --smart -V lang:russian --output="./export/$name.fb2" "./export/$name.md"
 "  * Архивируем FB2..."
 if(test-path($7z)) {
-   &$7z a -tzip -mx9 "./export/hpmor_ru.fb2.zip" "./export/hpmor_ru.fb2" 
+   &$7z a -tzip -mx9 "./export/$name.fb2.zip" "./export/$name.fb2" 
 } else {
-   zip -9 -m -D -j "./export/hpmor_ru.fb2.zip" "./export/hpmor_ru.fb2"
+   zip -9 -m -D -j "./export/$name.fb2.zip" "./export/$name.fb2"
 }
 "Конвертируем в DOCX..."
-&$pandoc --from=markdown --smart -V lang:russian --output=export/hpmor_ru.docx "./export/hpmor_ru_pandoc.md"
+&$pandoc --from=markdown --smart -V lang:russian --output="./export/$name.docx" "./export/$name.md"
 "Конвертируем в MOBI..."
-&$pandoc --from=markdown --smart -V lang:russian --output=export/hpmor_ru.mobi "./export/hpmor_ru_pandoc.md"
+&$pandoc --from=markdown --smart -V lang:russian --output="./export/$name.mobi" "./export/$name.md"
