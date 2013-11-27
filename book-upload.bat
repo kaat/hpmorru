862a12af5c8fe6cde9f2651e81fce5d0a52bbca4
Constantine <overlord1983@mail.ru>
2013-11-27 05:00:20 -0800

    [HPMOR] убран обескураживающий `export`, добавлен понятный `parts` + допилен скрипт выкладывания на сайт

--- book-upload.bat
+++ book-upload.bat
@@ -5,20 +5,18 @@
 @goto :EOF
 #>
 
-# набросок сие есмь
-
-$login = "ololo"
+$login = read-host -prompt "А введите-ка логин"
 $secure_password = read-host -prompt "А введите-ка пароль" -AsSecureString
-
 $ptr_password = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secure_password)
 $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto($ptr_password)
 [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ptr_password)
 
-$login
-$password
+&pscp -pw $password "./export/hpmor-feed.xml" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor-feed.xml
 
-&pscp -pw$secure_password "./export/hpmor_ru.fb2.zip" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor_ru.fb2.zip
-&pscp -pw$secure_password "./export/hpmor_ru.mobi" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor_ru.mobi
-&pscp -pw$secure_password "./export/hpmor_ru.epub" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor_ru.epub
-&pscp -pw$secure_password "./export/hpmor_ru.html" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor_ru.html
-&pscp -pw$secure_password "./export/hpmor-feed.xml" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor-feed.xml
+# выгружаем результат работы pandoc
+&pscp -pw $password "./export/hpmor_ru_pandoc.html" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor_ru.html
+
+# выгружаем результат работы calibre (!todo)
+# &pscp -pw $password "./export/hpmor_ru_calibre.fb2.zip" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor_ru.fb2.zip
+# &pscp -pw $password "./export/hpmor_ru_calibre.mobi" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor_ru.mobi
+# &pscp -pw $password "./export/hpmor_ru_calibre.epub" $login@hpmor.ru:/home/httpd/vhosts/hpmor.ru/httpdocs/files/hpmor_ru.epub
